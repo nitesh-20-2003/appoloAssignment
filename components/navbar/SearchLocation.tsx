94bdd77cc40459864e53006d8f1f239f5bebabd0
Nitesh Sharma <niteshsharmauiet@gmail.com>
2025-05-07 06:39:23 +0530

    loc1

--- components/navbar/SearchLocation.tsx
+++ components/navbar/SearchLocation.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { CiLocationOn } from "react-icons/ci";
-import { ComboboxDemo } from "./Loc";
+import { CityCombobox } from "./Loc";
 const SearchLocation = () => {
   return (
     <div className="flex items-center mb-5">
       <CiLocationOn className="h-10 w-10 mt-4" />
       <div className="flex flex-col items-center">
         <span className=" bolder text-gray-500"> Select Location</span>
-        <ComboboxDemo />
+        <CityCombobox />
       </div>
      
     </div>
